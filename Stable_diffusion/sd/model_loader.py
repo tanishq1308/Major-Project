b9d399109cb327c5061c85b7206d036af2963c1f
tanishqkakkar <kakkartanishq13@gmail.com>
2024-04-24 22:59:33 +0530

    add requirements

--- Stable_diffusion/sd/model_loader.py
+++ Stable_diffusion/sd/model_loader.py
@@ -1,5 +1,4 @@
 from clip import CLIP
-# from encoder import VAE_Encoder
 from decoder import VAE_Decoder
 from diffusion import Diffusion
 
--- Stable_diffusion/sd/pipeline.py
+++ Stable_diffusion/sd/pipeline.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 from tqdm import tqdm
 from ddpm import DDPMSampler
 
